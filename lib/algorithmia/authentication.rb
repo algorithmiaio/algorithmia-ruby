365a55210b3c27a8fcc880098710787ca4b267a7
Rush <lizr@algorithmia.com>
2016-03-15 17:40:23 -0700

    Clarify API key msising error

--- lib/algorithmia/authentication.rb
+++ lib/algorithmia/authentication.rb
@@ -11,7 +11,7 @@ module Algorithmia
 
     def self.api_key=(api_key)
       if not api_key.is_a?(String) or api_key.to_s.empty?
-        raise AlgorithmiaApiKeyEmpty.new('Looks like you forgot to provide your Algorithmia API key or it was invalid.')
+        raise AlgorithmiaApiKeyEmpty.new('Looks like you forgot to provide your Algorithmia API key. Please set `Algorithmia::Client.api_key`.')
       end
 
       @api_key = api_key
