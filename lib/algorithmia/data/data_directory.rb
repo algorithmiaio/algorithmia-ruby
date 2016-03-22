ed50ac0e8dcf3fe732e26c52b4f0d4f609f62087
liz rush <liz@lizmrush.com>
2016-03-22 16:17:07 -0700

    Iterate over files & dirs on Directories

--- lib/algorithmia/data/data_directory.rb
+++ lib/algorithmia/data/data_directory.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module Algorithmia
   class DataDirectory < DataObject
 
@@ -14,30 +16,56 @@ module Algorithmia
 
     def exists?
       Algorithmia::Http.new(@client).get(@url)
+      true
     end
 
     def create
     end
 
     def delete
+      # TODO: optional force
       Algorithmia::Http.new(@client).delete(@url, query: { force: :true })
     end
 
-    def getIterator
-      # getIterator should iterate over both files and directories
+    def each
+      return enum_for(:each) unless block_given?
+
+      dir = Algorithmia::Http.new(@client).get(@url)
+
+      files = (dir.parsed_response['files'] || []).map do |f|
+        file(f['filename'])
+      end
+
+      dirs = (dir.parsed_response['folders'] || []).map do |f|
+        @client.dir(URI.encode(@data_uri + f['name']))
+      end
+
+      (files + dirs).each do |f|
+        yield f
+      end
     end
 
-    def getFileIterator
-      # getFileIterator should iterate over only files (skipping any directories)
+    def each_file
+      return enum_for(:each_file) unless block_given?
+
+      dir = Algorithmia::Http.new(@client).get(@url)
+      (dir.parsed_response['files'] || []).each do |f|
+        yield file(f['filename'])
+      end
     end
 
-    def getDirIterator
-      # getDirIterator should iterate over only directories (skipping any files)
+    def each_dir
+      return enum_for(:each_dir) unless block_given?
+
+      dir = Algorithmia::Http.new(@client).get(@url)
+      (dir.parsed_response['folders'] || []).each do |f|
+        yield @client.dir(URI.encode(@data_uri + f['name']))
+      end
     end
 
     def file(file_name)
       # TODO: check if filename has leading slash; if not, add it
-      @client.file(@data_uri + file_name)
+      @client.file(URI.encode(@data_uri + file_name))
     end
 
     def put_file(file_path)
