c6dd4cf574601b3a7040b07651487500bc42fcaf
Anthony Nowell <anthony@algorithmia.com>
2016-04-11 11:10:13 -0600

    Cleanup binary test

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -23,11 +23,10 @@ describe Algorithmia::Algorithm do
 
   it 'can make binary api call' do
     image_in = __dir__+'/data/theoffice.jpg'
-    image_out = __dir__+'/data/theoffice_thumb.png'
     input = File.binread(image_in)
-    expected_sha = "1e8042669864e34a65ba05b1c457e24aab7184dd5990c9377791e37890ac8760"
-
     response = client.algo("opencv/SmartThumbnail/0.1.14").pipe(input)
+
+    expected_sha = "1e8042669864e34a65ba05b1c457e24aab7184dd5990c9377791e37890ac8760"
     expect(response.content_type).to eq('binary')
     expect(response.result.encoding).to eq(Encoding::ASCII_8BIT)
     expect(Digest::SHA256.hexdigest(response.result)).to eq(expected_sha)
