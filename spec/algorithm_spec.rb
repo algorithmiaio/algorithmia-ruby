720b2b99363e1d091e8dc69e810aee47abe88c9d
jakemuntarsi <71137150+jakemuntarsi@users.noreply.github.com>
2021-03-31 10:42:56 -0400

    DEV-231: org_url is mandatory for creating organization (#38)
    * DEV-231: org_url is mandatory for creating organization
    
    * DEV-231: org_url is mandatory for creating organization & fixing the failing test cases

--- spec/algorithm_spec.rb
+++ spec/algorithm_spec.rb
@@ -80,9 +80,8 @@ describe Algorithmia::Algorithm do
                               org_label: "myLabel",
                               org_contact_name: "some owner",
                               org_email: "#{org_name}@algo.com",
+                              org_url: "https://#{org_name}algo.com",
                               external_id: "ext_#{time_now}",
-                              external_admin_group: "ext_admin_#{time_now}",
-                              external_member_group: "ext_member_#{time_now}",
                               type_id: "basic")
 
     result = test_admin.create_organization(organization.to_h.to_json)
@@ -90,9 +89,8 @@ describe Algorithmia::Algorithm do
     expect(result.response["org_label"]).to eq("myLabel")
     expect(result.response["org_contact_name"]).to eq("some owner")
     expect(result.response["org_email"]).to eq("#{org_name}@algo.com")
+    expect(result.response["org_url"]).to eq("https://#{org_name}algo.com")
     expect(result.response["external_id"]).to eq("ext_#{time_now}")
-    expect(result.response["external_admin_group"]).to eq("ext_admin_#{time_now}")
-    expect(result.response["external_member_group"]).to eq("ext_member_#{time_now}")
     destroy_test_org org_name
   end
 
